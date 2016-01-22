a7e7cc1be306f399e2c668864966f6341b647d61
root <root@ueb.ir.vhebron.net>
2016-01-22 13:53:05 +0100

    Added hidden folder .b52 to keep custom icons such as IGV's and tweaked background image file for the desktop with user's username shown on it. Tweaked addb52user.sh accordingly

--- addb52user.sh
+++ addb52user.sh
@@ -43,14 +43,15 @@ echo "export PATH=$PATH:/usr/local/stata" >> /home/$newuser/.bashrc
 echo "* His/her .bashrc tweaked..."
 cd /home/ueb/scripts/templates4systemusers;cp --parents Desktop/*.desktop /home/$newuser/;cd ~
 echo "* Desktop shortcuts added..."
-cd /home/ueb;cp --parents .java/deployment/cache/6.0/35/442040a3-1e81535d.ico /home/$newuser/;cd ~
-echo "* Desktop icons for the previous shortcuts provided..."
+cd /home/ueb/scripts/templates4systemusers;cp --parents .b52/* /home/$newuser/;cd ~
+echo "* Desktop icon and background files copied to the "$newuser" folders..."
 cd /home/ueb/scripts/templates4systemusers;cp --parents .config/lxsession/LXDE/autostart /home/$newuser/;cd ~
 echo "* Keyboard fixed..."
 cd /home/ueb/scripts/templates4systemusers;cp --parents .config/pcmanfm/LXDE/*.conf /home/$newuser/;cd ~
 echo "* Desktop background changed..."
 sed -i -e "s/foo.bar/$newuser/g" /home/$newuser/Desktop/*.desktop
-echo "* Fixed paths for "$newuser!" in desktop icons..."
+sed -i -e "s/foo.bar/$newuser/g" /home/$newuser/.b52/*.svg
+echo "* Fixed paths and name for "$newuser" in desktop icons and background..."
 cd /home/$newuser/;chown $newuser:$newuser Desktop Desktop/* -R;cd ~
 cd /home/$newuser/;chown $newuser:$newuser .config .config/* -R;cd ~
 echo "* Ownership and permissions of new folders and files fixed..."
