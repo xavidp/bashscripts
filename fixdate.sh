66182be3848f8bae33fc1c8188115077fdd4d682
F692118 <xdepedro@bcn.cat>
2020-01-02 17:50:20 +0100

    updated url to google to use https since http since to be moved permanently Error 301

--- fixdate.sh
+++ fixdate.sh
@@ -1,3 +1,3 @@
 date
-sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z" | sudo hwclock --systohc
+sudo date -s "$(wget -qSO- --max-redirect=0 https://google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z" | sudo hwclock --systohc
 date
