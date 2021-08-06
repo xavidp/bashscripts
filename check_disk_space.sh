34ecbb9ce88986c6c78c87944cbddae0f3649d17
Xavier de Pedro Puente <xavidp@users.noreply.github.com>
2021-08-06 13:44:23 +0200

    first version - working already

--- check_disk_space.sh
+++ check_disk_space.sh
@@ -1,15 +1,18 @@
 #/bin/bash
-#ADMIN="ueb@ir.vhebron.net"
-#ADMIN="xavier.depedro@vhir.org,ueb@vhir.org"
-ADMIN="xavier.depedro@vhir.org"
+SMTP="localhost"
+EMAILF="xdepedro@bcn.cat"
+#EMAILT="xdepedro@bcn.cat"
+#EMAILT="xdepedro@bcn.cat,omd-gid@bcn.cat"
+EMAILT="xdepedro@bcn.cat,asabater@bcn.cat,omd-gid@bcn.cat"
 # set alert-level on a given %
-ALERT=82
-df -H 2> /dev/null | grep -vE '^Filesystem|tmpfs|cdrom|MainHead|magatzem|S\.\ fitxers' | awk '{ print $5 " " $6 }' | while read output;
+ALERT=99
+df -H 2> /dev/null | grep -vE '^Filesystem|tmpfs|cdrom|dev$|snap|sys|run|docker|windows/o|S\.\ fitxers' | awk '{ print $5 " " $6 }' | while read output;
 do
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo $output | awk '{ print $2 }' )
   if [ $usep -ge $ALERT ]; then
-    echo "space low on \"$partition ($usep%)\", on server $(hostname) at $(date)" |
-    mail -s "Alert: Free space low at B52, $usep % used on $partition" $ADMIN
+#    echo "space low on \"$partition ($usep%)\", on server $(hostname) at $(date)" > last_alert.txt
+#    echo "space low on \"$partition ($usep%)\", on server $(hostname) at $(date +\"%Y-%m-%d\")" | sendemail -f $EMAILF -t $EMAILT -u 'Alert: Free space low at OMD-GID resources, $usep % used on $partition' -a last_alert.txt -s $SMTP -o tls=no
+    echo "space low on \"$partition ($usep%)\", on server $(hostname) at $(date +\"%Y-%m-%d\ %H:%M\")" | sendemail -f $EMAILF -t $EMAILT -u '[Alert][ScrumWorker]: disk filling up' -s $SMTP -o tls=no
  fi
 done
